360a61f0f8f737c3c2503fa25b2b5d4960b50def
Boaca Lucian <lucianboaca97@gmail.com>
2019-03-20 18:38:47 +0200

    Add project files.

---
+++ ALGroups/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ALGroups.MvcApplication" Language="C#" %>
---
+++ ALGroups/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ALGroups.Startup))]
+namespace ALGroups
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
