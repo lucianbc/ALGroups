96cd24dcfc988c1c074b7a40316f1d35d3d1fdd5
Boaca Lucian <lucianboaca97@gmail.com>
2019-04-06 14:39:05 +0300

    Removed the css and js dependencies

--- ALGroups/App_Start/BundleConfig.cs
+++ ALGroups/App_Start/BundleConfig.cs
@@ -14,12 +14,12 @@ namespace ALGroups
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*",
                         "~/Scripts/moment.js",
-                        "~/Scripts/user/validations.js"));
+                        "~/Scripts/scripts/validations.js"));
             bundles.Add(new ScriptBundle("~/bundles/calendar").Include(
                         "~/Scripts/moment.js",
                         "~/Scripts/fullcalendar.js",
                         "~/Scripts/qTip/jquery.qtip.js",
-                        "~/Scripts/user/mycalendar.js"));
+                        "~/Scripts/scripts/mycalendar.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -31,11 +31,11 @@ namespace ALGroups
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/Site.css"));
+                      "~/Content/style/Site.css"));
             bundles.Add(new StyleBundle("~/Content/groupcss").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/Site.css",
-                      "~/Content/navbar-fixed-left.css",
+                      "~/Content/style/Site.css",
+                      "~/Content/style/navbar-fixed-left.css",
                       //"~/Content/fullcalendar.print.css",
                       "~/Content/fullcalendar.css",
                       "~/Scripts/qTip/jquery.qtip.css"
